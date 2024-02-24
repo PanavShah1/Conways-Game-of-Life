41a81602249160c4eb65408883c048bad58dac15
PanavShah1 <panav.shah@gmail.com>
2024-02-24 15:18:29 +0530

    created cellular automata

---
+++ configurations.md
@@ -0,0 +1,38 @@
+    cells_array[15][15].fill = true
+    cells_array[16][15].fill = true
+    cells_array[17][15].fill = true
+    cells_array[15][16].fill = true
+
+veritasium
+    cells_array[15][15].fill = true
+    cells_array[16][15].fill = true
+    cells_array[16][16].fill = true
+    cells_array[17][16].fill = true
+    cells_array[17][17].fill = true
+    cells_array[17][18].fill = true
+    cells_array[18][18].fill = true
+
+glider
+    cells_array[15][15].fill = true
+    cells_array[16][14].fill = true
+    cells_array[17][14].fill = true
+    cells_array[17][15].fill = true
+    cells_array[17][16].fill = true
+
+repeat
+    cells_array[15][15].fill = true
+    cells_array[16][15].fill = true
+    cells_array[17][15].fill = true
+    cells_array[16][14].fill = true
+
+still
+    cells_array[15][15].fill = true
+    cells_array[15][14].fill = true
+    cells_array[16][15].fill = true
+    cells_array[17][14].fill = true
+    cells_array[18][13].fill = true
+    cells_array[19][12].fill = true
+    cells_array[19][11].fill = true
+    cells_array[18][11].fill = true
+
+
---
+++ index.js
@@ -0,0 +1,157 @@
+const canvas = document.getElementById("canvas")
+
+const ctx = canvas.getContext("2d")
+
+const W = canvas.width
+const H = canvas.height
+const N = 40
+const W_cell = W/N
+const H_cell = H/N
+
+console.log(H)
+console.log(W)
+console.log("hi")
+
+let cells_array = []
+class Cell{
+    constructor(i, j){
+        this.x = i*W_cell
+        this.y = j*H_cell
+        this.fill = false
+    }
+}
+
+for(let i = 0; i < N; i++){
+    let temp_array = []
+    for(let j = 0; j < N; j++){
+        temp_array.push(new Cell(i, j))
+    }
+    cells_array.push(temp_array)
+}
+
+console.log(cells_array)
+
+function randomCells(event){
+    for(let i = 0; i < N; i++){
+        for(let j = 0; j < N; j++){
+            var rand = Math.floor(Math.random()*10)
+            if(rand <= 3){
+                cells_array[i][j].fill = true
+            }
+            else{
+                cells_array[i][j].fill = false
+            }
+        }
+    }
+    fillColor()
+}
+
+// function randomCells(event){
+//     cells_array[15][15].fill = true
+//     cells_array[16][14].fill = true
+//     cells_array[17][14].fill = true
+//     cells_array[17][15].fill = true
+//     cells_array[17][16].fill = true
+
+//     fillColor()
+
+// }
+
+
+function fillColor(){
+    ctx.clearRect(0, 0, W, H)
+    for(let i = 0; i < N; i++){
+        for(let j = 0; j < N; j++){
+            if(cells_array[i][j].fill == true){
+                ctx.beginPath()
+                ctx.fillStyle = "white"
+                // console.log("true")
+                ctx.rect(cells_array[i][j].x, cells_array[i][j].y, cells_array[i][j].x+W_cell, cells_array[i][j].y+H_cell)
+                ctx.fill()
+                ctx.closePath();
+            }
+            else{
+                ctx.beginPath()
+                ctx.fillStyle = "black"
+                // console.log("false")
+                ctx.rect(cells_array[i][j].x, cells_array[i][j].y, cells_array[i][j].x+W_cell, cells_array[i][j].y+H_cell)
+                ctx.fill()
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+function changeCells(){
+    
+    let duplicate_cells_array = []
+    function duplicate() {
+        duplicate_cells_array = cells_array.map(row => row.map(cell => ({...cell})));
+    }
+    duplicate()
+
+    console.log("changing cells")
+    for(let i = 1; i < N-1; i++){
+        for(let j = 1; j < N-1; j++){
+            var ctr = 0
+            
+            for(let ii = i-1; ii <= i+1; ii++){
+                for(let jj = j-1; jj <= j+1; jj++){
+                    if(cells_array[ii][jj].fill == true){
+                        ctr++
+                    }
+                }
+            }
+
+            if(cells_array[i][j].fill == true){
+                ctr--
+            }
+
+
+
+
+
+
+
+            if(cells_array[i][j].fill == true){
+                if(ctr==2||ctr==3){
+                    duplicate_cells_array[i][j].fill = true
+                }
+                else{
+                    duplicate_cells_array[i][j].fill = false
+                }
+            }
+
+            if(cells_array[i][j].fill == false){
+                if(ctr == 3){
+                    duplicate_cells_array[i][j].fill = true
+                }
+                else{
+                    duplicate_cells_array[i][j].fill = false
+                }
+            }
+
+        }
+    }
+
+    function duplicate2() {
+        cells_array = duplicate_cells_array.map(row => row.map(cell => ({...cell})));
+    }
+    duplicate2()
+
+    fillColor();
+}
+
+// function run(){
+//     console.log("running")
+//     randomCells()
+//     fillColor()
+//     console.log(cells_array)
+//     console.log("finished running")
+// }
+
+// run()
+document.addEventListener("DOMContentLoaded", randomCells)
+
+setInterval(changeCells, 200)
+
